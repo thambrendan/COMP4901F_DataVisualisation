525b5d6366da4c92938156a7f2baf5a066c7806a
arcross4682 <33666612+arcross4682@users.noreply.github.com>
2018-05-13 22:12:23 +0800

    testing bootstrap

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ def visualisation_1():
 @app.route("/visualisation_3")
 def visualisation_3():
     return render_template('visualisation_3.html')
+
+@app.route("/testing")
+def visualisation_3():
+    return render_template('bootstrap2.html')
  
 if __name__ == "__main__":
 	app.static_folder = 'static'
