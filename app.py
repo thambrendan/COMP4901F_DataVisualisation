df671a6149b05c1f589a4a3a6aa7205272a7b347
unknown <tunjian1996@gmail.com>
2018-05-13 16:08:36 +0800

    V2 10%

--- app.py
+++ app.py
@@ -3,11 +3,15 @@ app = Flask(__name__)
  
 @app.route("/")
 def overview_home():
-    return render_template('visualisation_3.html')
+    return render_template('visualisation_2.html')
 
-@app.route("/overview")
-def overview():
+@app.route("/visualisation_1")
+def visualisation_1():
     return render_template('visualisation_1.html')
+
+@app.route("/visualisation_3")
+def visualisation_3():
+    return render_template('visualisation_3.html')
  
 if __name__ == "__main__":
 	app.static_folder = 'static'
