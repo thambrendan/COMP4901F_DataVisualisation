3d5d341207f8c13194011f1904ef885f0b5ef530
arcross4682 <33666612+arcross4682@users.noreply.github.com>
2018-05-14 00:32:09 +0800

    update

--- app.py
+++ app.py
@@ -13,6 +13,10 @@ def visualisation_1():
 def visualisation_3():
     return render_template('visualisation_3.html')
 
+@app.route("/main")
+def testing_bootstrap():
+    return render_template('bootstrap2.html')
+
 @app.route("/testing")
 def testing_bootstrap():
     return render_template('bootstrap3.html')
